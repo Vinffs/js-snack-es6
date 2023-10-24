d135549a014a177466418cbeb9e50d27e8572c5d
Vinffs <vin.demiranda@gmail.com>
2023-10-24 14:10:47 +0200

    created list of students

--- script.js
+++ script.js
@@ -21,3 +21,43 @@ const toPrint = invitedVip.map((name, index) => ({
 }));
 
 console.log(toPrint);
+
+// SNACK 2
+
+const students = [
+  {
+    id: "213",
+    fullName: "Marco della Rovere",
+    grade: "78",
+  },
+  {
+    id: "110",
+    fullName: "Paola Cortellessa",
+    grade: "96",
+  },
+  {
+    id: "250",
+    fullName: "Andrea Mantegna",
+    grade: "48",
+  },
+  {
+    id: "145",
+    fullName: "Gaia Borromini",
+    grade: "74",
+  },
+  {
+    id: "196",
+    fullName: "Luigi Grimaldello",
+    grade: "68",
+  },
+  {
+    id: "102",
+    fullName: "Piero della Francesca",
+    grade: "50",
+  },
+  {
+    id: "120",
+    fullName: "Francesca da Polenta",
+    grade: "84",
+  },
+];
